f6bb92eb34e1c10aa58860e1ad645f8e4d66b55d
ilahaka77 <ahmadarifilahaka77@gmail.com>
2021-02-11 13:30:57 +0700

    menambahkan validasi beserta responsenya

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
@@ -29,6 +30,17 @@ class PostController extends Controller
     public function store(Request $request)
     {
         $data = $request->all();
+
+        $validator = Validator::make($data,[
+            'title' => ['required', 'min:5']
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'error' => $validator->errors()
+            ]);
+        }
+
         Post::create($data);
         return response()->json('success', 201);
     }
